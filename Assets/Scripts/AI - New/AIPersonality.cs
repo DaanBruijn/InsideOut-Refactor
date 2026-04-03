bff25b869bb98f1624148771015a11b9286ace5a
Daniël Bruijn <bruijndaniel1308@gmail.com>
2026-04-03 13:42:08 +0200

    Final push for assignment
    Changed color of one of the cars and balanced the AI.
    
    -Different AI car added to make it more distinct
    -Changed AI speed modifier from max 2 to 1.2

--- Assets/Scripts/AI - New/AIPersonality.cs
+++ Assets/Scripts/AI - New/AIPersonality.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [System.Serializable]
 public class AIPersonality
 {
-    [Range(0.5f, 2f)] public float speedMultiplier = 1f;
+    [Range(0.5f, 1.2f)] public float speedMultiplier = 1f;
     [Range(0.5f, 2f)] public float aggression = 1f;
     [Range(0.5f, 2f)] public float corneringSkill = 1f;
     [Range(0f, 1f)] public float avoidanceStrength = 1f;
